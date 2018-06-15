c763082c7dcf1de02af5faf444c2f6f3b62694a0
garpik <garpik@tut.by>
2018-06-15 11:00:12 +0200

    shipping address validation

--- Collector/Iframe/view/frontend/web/js/cart.js
+++ Collector/Iframe/view/frontend/web/js/cart.js
@@ -1,8 +1,9 @@
 define([
-	'jquery'
-], function ($, collectorajax) {
-	return {
-		call:function(ajaxUrl){
+    'jquery',
+    'mage/validation'
+], function ($, collectorajax, validation) {
+    return {
+        call: function (ajaxUrl) {
             $(document).on('change', '.collector_shipping_address input, .collector_shipping_address select', function () {
                 var param = {
                     is_ajax: true,
@@ -10,17 +11,64 @@ define([
                     name: $(this).attr('name'),
                     value: $(this).val()
                 }
+
+                if (!$('.form-shipping-address').valid()) {
+                    $('div.mage-error').remove();
+                    $('.collector-checkout-wrapper').addClass('disabled');
+                }
+                else {
+                    $('.collector-checkout-wrapper').removeClass('disabled');
+                }
+
+                if ($(this).hasClass('validate')) {
+                    $(this).mage('validation', {});
+                }
+                if ($(this).valid()) {
+                    $.ajax({
+                        url: ajaxUrl,
+                        data: param,
+                        type: "POST",
+                        dataType: 'json',
+                        beforeSend: function () {
+                            //jQuery('body').addClass('is-suspended');
+                            window.collector.checkout.api.suspend();
+                        },
+                        complete: function () {
+                            //jQuery('body').removeClass('is-suspended');
+                            window.collector.checkout.api.resume();
+                            require([
+                                'Magento_Customer/js/customer-data'
+                            ], function (customerData) {
+                                var sections = ['cart'];
+                                customerData.invalidate(sections);
+                                customerData.reload(sections, true);
+                            });
+                        }
+                    });
+                }
+            });
+            $(document).on('click', '.col-inc', function () {
+                var param = {
+                    is_ajax: true,
+                    id: this.id,
+                    type: "inc"
+                };
                 $.ajax({
                     url: ajaxUrl,
                     data: param,
                     type: "POST",
                     dataType: 'json',
                     beforeSend: function () {
-                        //jQuery('body').addClass('is-suspended');
+                        jQuery('body').addClass('is-suspended');
                         window.collector.checkout.api.suspend();
                     },
+                    success: function (data) {
+                        if (data.cart) {
+                            jQuery('div.collector-cart').replaceWith(data.cart);
+                        }
+                    },
                     complete: function () {
-                        //jQuery('body').removeClass('is-suspended');
+                        jQuery('body').removeClass('is-suspended');
                         window.collector.checkout.api.resume();
                         require([
                             'Magento_Customer/js/customer-data'
@@ -32,247 +80,214 @@ define([
                     }
                 });
             });
-			$(document).on('click', '.col-inc', function() {
-				var param = {
-					is_ajax: true,
-                    id: this.id,
-                    type: "inc"
-				};
-				$.ajax({
-					url: ajaxUrl,
-					data: param,
-					type: "POST",
-					dataType: 'json',
-					beforeSend: function() {
-						jQuery('body').addClass('is-suspended');
-						window.collector.checkout.api.suspend();
-					},					   
-					success: function(data) {
-						if(data.cart){
-							jQuery('div.collector-cart').replaceWith(data.cart);
-						}
-					},
-					complete: function() {
-						jQuery('body').removeClass('is-suspended');
-						window.collector.checkout.api.resume();
-						require([
-							'Magento_Customer/js/customer-data'
-						], function (customerData) {
-							var sections = ['cart'];
-							customerData.invalidate(sections);
-							customerData.reload(sections, true);
-						});
-					}
-				});
-			});
-			$(document).on('click', '.col-sub', function() {
-				var param = {
+            $(document).on('click', '.col-sub', function () {
+                var param = {
                     is_ajax: true,
                     id: this.id,
                     type: "sub"
-				};
-				$.ajax({
-					url: ajaxUrl,
-					data: param,
-					type: "POST",
-					dataType: 'json',
-					beforeSend: function() {
-						jQuery('body').addClass('is-suspended');
-						window.collector.checkout.api.suspend();
-					},					   
-					success: function(data) {
-						if(data.cart){
-							jQuery('div.collector-cart').replaceWith(data.cart);
-						}
-					},
-					complete: function() {
-						jQuery('body').removeClass('is-suspended');
-						window.collector.checkout.api.resume();
-						require([
-							'Magento_Customer/js/customer-data'
-						], function (customerData) {
-							var sections = ['cart'];
-							customerData.invalidate(sections);
-							customerData.reload(sections, true);
-						});
-					}
-				});
-			});
-			$(document).on('click', '.newsletter', function(){
-				var param = {
+                };
+                $.ajax({
+                    url: ajaxUrl,
+                    data: param,
+                    type: "POST",
+                    dataType: 'json',
+                    beforeSend: function () {
+                        jQuery('body').addClass('is-suspended');
+                        window.collector.checkout.api.suspend();
+                    },
+                    success: function (data) {
+                        if (data.cart) {
+                            jQuery('div.collector-cart').replaceWith(data.cart);
+                        }
+                    },
+                    complete: function () {
+                        jQuery('body').removeClass('is-suspended');
+                        window.collector.checkout.api.resume();
+                        require([
+                            'Magento_Customer/js/customer-data'
+                        ], function (customerData) {
+                            var sections = ['cart'];
+                            customerData.invalidate(sections);
+                            customerData.reload(sections, true);
+                        });
+                    }
+                });
+            });
+            $(document).on('click', '.newsletter', function () {
+                var param = {
                     is_ajax: true,
                     value: document.getElementById('newsletter-checkbox').checked,
                     type: "newsletter"
-				};
-				$.ajax({
-					url: ajaxUrl,
-					data: param,
-					type: "POST",
-					dataType: 'json',
-					beforeSend: function(){
-						jQuery('body').addClass('is-suspended');
-						window.collector.checkout.api.suspend();
-					},
-					success: function(data){
-						
-					},
-					complete: function(){
-						jQuery('body').removeClass('is-suspended');
-						window.collector.checkout.api.resume();
-					}
-				});
-			});
-			$(document).on('click', '.col-del', function() {
-				var param = {
+                };
+                $.ajax({
+                    url: ajaxUrl,
+                    data: param,
+                    type: "POST",
+                    dataType: 'json',
+                    beforeSend: function () {
+                        jQuery('body').addClass('is-suspended');
+                        window.collector.checkout.api.suspend();
+                    },
+                    success: function (data) {
+
+                    },
+                    complete: function () {
+                        jQuery('body').removeClass('is-suspended');
+                        window.collector.checkout.api.resume();
+                    }
+                });
+            });
+            $(document).on('click', '.col-del', function () {
+                var param = {
                     is_ajax: true,
                     id: this.id,
                     type: "del"
-				};
-				$.ajax({
-					url: ajaxUrl,
-					data: param,
-					type: "POST",
-					dataType: 'json',
-					beforeSend: function() {
-						jQuery('body').addClass('is-suspended');
-						window.collector.checkout.api.suspend();
-					},					   
-					success: function(data) {
-						if (data == "redirect"){
-							require([
-							'Magento_Customer/js/customer-data'
-						], function (customerData) {
-							var sections = ['cart'];
-							customerData.invalidate(sections);
-							customerData.reload(sections, true);
-						});
-							window.location.href = window.location.protocol + "//" + window.location.host + "/";
-						}
-						if(data.cart){
-							jQuery('div.collector-cart').replaceWith(data.cart);
-						}
-					},
-					complete: function() {
-						jQuery('body').removeClass('is-suspended');
-						window.collector.checkout.api.resume();
-						require([
-							'Magento_Customer/js/customer-data'
-						], function (customerData) {
-							var sections = ['cart'];
-							customerData.invalidate(sections);
-							customerData.reload(sections, true);
-						});
-					}
-				});
-			});
-			$(document).on('click', '.col-radio', function() {
-				var param = {
+                };
+                $.ajax({
+                    url: ajaxUrl,
+                    data: param,
+                    type: "POST",
+                    dataType: 'json',
+                    beforeSend: function () {
+                        jQuery('body').addClass('is-suspended');
+                        window.collector.checkout.api.suspend();
+                    },
+                    success: function (data) {
+                        if (data == "redirect") {
+                            require([
+                                'Magento_Customer/js/customer-data'
+                            ], function (customerData) {
+                                var sections = ['cart'];
+                                customerData.invalidate(sections);
+                                customerData.reload(sections, true);
+                            });
+                            window.location.href = window.location.protocol + "//" + window.location.host + "/";
+                        }
+                        if (data.cart) {
+                            jQuery('div.collector-cart').replaceWith(data.cart);
+                        }
+                    },
+                    complete: function () {
+                        jQuery('body').removeClass('is-suspended');
+                        window.collector.checkout.api.resume();
+                        require([
+                            'Magento_Customer/js/customer-data'
+                        ], function (customerData) {
+                            var sections = ['cart'];
+                            customerData.invalidate(sections);
+                            customerData.reload(sections, true);
+                        });
+                    }
+                });
+            });
+            $(document).on('click', '.col-radio', function () {
+                var param = {
                     is_ajax: true,
                     id: this.id,
                     type: "radio"
-				};
-				$.ajax({
-					url: ajaxUrl,
-					data: param,
-					type: "POST",
-					dataType: 'json',
-					beforeSend: function() {
-						// Suspend the Checkout, showing a spinner...
-						jQuery('body').addClass('is-suspended');
-						window.collector.checkout.api.suspend();
-					},
-					success:function(data){
-						if(data.cart){
-							jQuery('div.collector-cart').replaceWith(data.cart);
-						}
-						if(data.checkout){
-							jQuery('div.collector-checkout').replaceWith(data.checkout);
-						}
-					},
-					complete: function() {
-						// ... and finally resume the Checkout after the backend call is completed to update the checkout
-						jQuery('body').removeClass('is-suspended');
-						window.collector.checkout.api.resume();
-					},
-				});
-			});
-			$(document).on('click', '.col-codeButton', function(){
-				var param = {
+                };
+                $.ajax({
+                    url: ajaxUrl,
+                    data: param,
+                    type: "POST",
+                    dataType: 'json',
+                    beforeSend: function () {
+                        // Suspend the Checkout, showing a spinner...
+                        jQuery('body').addClass('is-suspended');
+                        window.collector.checkout.api.suspend();
+                    },
+                    success: function (data) {
+                        if (data.cart) {
+                            jQuery('div.collector-cart').replaceWith(data.cart);
+                        }
+                        if (data.checkout) {
+                            jQuery('div.collector-checkout').replaceWith(data.checkout);
+                        }
+                    },
+                    complete: function () {
+                        // ... and finally resume the Checkout after the backend call is completed to update the checkout
+                        jQuery('body').removeClass('is-suspended');
+                        window.collector.checkout.api.resume();
+                    },
+                });
+            });
+            $(document).on('click', '.col-codeButton', function () {
+                var param = {
                     is_ajax: true,
                     value: document.getElementById("col-code").value,
                     type: "submit"
-				};
-				$.ajax({
-					url: ajaxUrl,
-					data: param,
-					type: "POST",
-					dataType: 'json',
-					beforeSend: function() {
-						jQuery('body').addClass('is-suspended');
-						window.collector.checkout.api.suspend();
-					},					   
-					success: function(data) {
-						if(data.cart){
-							jQuery('div.collector-cart').replaceWith(data.cart);
-						}
-					},
-					complete: function() {
-						jQuery('body').removeClass('is-suspended');
-						window.collector.checkout.api.resume();
-						require([
-							'Magento_Customer/js/customer-data'
-						], function (customerData) {
-							var sections = ['cart'];
-							customerData.invalidate(sections);
-							customerData.reload(sections, true);
-						});
-					}
-				});
-			});
-			$(document).on('click', '#col-businesstypes a', function(e) {
-				e.preventDefault();
-				var ctype = jQuery(this).attr('id');
-				jQuery.ajax({
-					url:ajaxUrl,
-					type:'POST',
-					dataType: 'json',
-					data: {
+                };
+                $.ajax({
+                    url: ajaxUrl,
+                    data: param,
+                    type: "POST",
+                    dataType: 'json',
+                    beforeSend: function () {
+                        jQuery('body').addClass('is-suspended');
+                        window.collector.checkout.api.suspend();
+                    },
+                    success: function (data) {
+                        if (data.cart) {
+                            jQuery('div.collector-cart').replaceWith(data.cart);
+                        }
+                    },
+                    complete: function () {
+                        jQuery('body').removeClass('is-suspended');
+                        window.collector.checkout.api.resume();
+                        require([
+                            'Magento_Customer/js/customer-data'
+                        ], function (customerData) {
+                            var sections = ['cart'];
+                            customerData.invalidate(sections);
+                            customerData.reload(sections, true);
+                        });
+                    }
+                });
+            });
+            $(document).on('click', '#col-businesstypes a', function (e) {
+                e.preventDefault();
+                var ctype = jQuery(this).attr('id');
+                jQuery.ajax({
+                    url: ajaxUrl,
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {
                         is_ajax: true,
                         value: ctype,
                         type: "btype"
-					},
-					beforeSend: function() {
-						// Suspend the Checkout, showing a spinner...
-						jQuery('body').addClass('is-suspended');
-						window.collector.checkout.api.suspend();
-					},
-					success:function(data){
-						if(data.cart){
-							jQuery('div.collector-cart').replaceWith(data.cart);
-						}
-						if(data.checkout){
-							jQuery('div.collector-checkout').replaceWith(data.checkout);
-						}
-						if (ctype == "b2b"){
-							jQuery("#b2c").addClass("col-inactive");
-							jQuery("#b2c").removeClass("col-active");
-							jQuery("#b2b").addClass("col-active");
-							jQuery("#b2b").removeClass("col-inactive");
-						}
-						else if (ctype == "b2c"){
-							jQuery("#b2b").addClass("col-inactive");
-							jQuery("#b2b").removeClass("col-active");
-							jQuery("#b2c").addClass("col-active");
-							jQuery("#b2c").removeClass("col-inactive");
-						}					
-					},
-					complete: function() {
-						// ... and finally resume the Checkout after the backend call is completed to update the checkout
-						jQuery('body').removeClass('is-suspended');
-						window.collector.checkout.api.resume();
-					},
-				});
-			});
-		}
-	}
+                    },
+                    beforeSend: function () {
+                        // Suspend the Checkout, showing a spinner...
+                        jQuery('body').addClass('is-suspended');
+                        window.collector.checkout.api.suspend();
+                    },
+                    success: function (data) {
+                        if (data.cart) {
+                            jQuery('div.collector-cart').replaceWith(data.cart);
+                        }
+                        if (data.checkout) {
+                            jQuery('div.collector-checkout').replaceWith(data.checkout);
+                        }
+                        if (ctype == "b2b") {
+                            jQuery("#b2c").addClass("col-inactive");
+                            jQuery("#b2c").removeClass("col-active");
+                            jQuery("#b2b").addClass("col-active");
+                            jQuery("#b2b").removeClass("col-inactive");
+                        }
+                        else if (ctype == "b2c") {
+                            jQuery("#b2b").addClass("col-inactive");
+                            jQuery("#b2b").removeClass("col-active");
+                            jQuery("#b2c").addClass("col-active");
+                            jQuery("#b2c").removeClass("col-inactive");
+                        }
+                    },
+                    complete: function () {
+                        // ... and finally resume the Checkout after the backend call is completed to update the checkout
+                        jQuery('body').removeClass('is-suspended');
+                        window.collector.checkout.api.resume();
+                    },
+                });
+            });
+        }
+    }
 });
